5279ca0c3b961a42b222ed882f3528a466635e22
Juraj Lapcak <lapcakjuraj@gmail.com>
2021-04-07 13:12:36 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<html lang="sk">
+<head>
+    <title>sse</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Juraj Lapčák">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <link href="/sse/assets/css/style.css" rel="stylesheet">
+    <script src="/sse/assets/js/script.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row mt-5">
+        <div class="col-lg ">
+            <main class="site-content">
+
+                <!-- TODO: MAIN CONTENT -->
+
+            </main>
+        </div>
+    </div>
+</div>
+
+<?php include(__DIR__ . "/partials/footer.php"); ?>
+</body>
